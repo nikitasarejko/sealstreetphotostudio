5a339ea350dbe4bf993edcab60c639cd4b2f47e6
Nikita Sarejko <n.sarejko@gmail.com>
2021-01-06 12:20:11 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+const sections = document.querySelectorAll('section')
+
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (entry.intersectionRatio > 0.1) {
+			entry.target.classList.add('in-view')
+		} else {
+			entry.target.classList.remove('in-view')
+		}
+	})
+}, {
+	threshold: [0.0, 0.1, 1.0]
+})
+
+sections.forEach(section => {
+	observer.observe(section)
+
+	const div = section.querySelector('div')
+	const mq = window.matchMedia('(prefers-reduced-motion: no-preference)')
+
+	if (mq.matches) {
+		document.addEventListener('mousemove', function(event) {
+			const aimX = (event.clientX - (window.innerWidth / 2)) / 25
+			const aimY = (event.clientY - (window.innerHeight / 2)) / -25
+
+			div.style.transform = `rotateX(${aimY}deg) rotateY(${aimX}deg)`
+		})
+	}
+})
